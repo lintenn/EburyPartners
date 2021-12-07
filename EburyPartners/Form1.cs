93efe38d0bd69ffe7ec83b765da5ff5e7d333d45
Galo <galopg2001@hotmail.com>
2021-12-07 17:48:48 +0100

    Menu

--- EburyPartners/Form1.cs
+++ EburyPartners/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace EburyPartners
 {
-    public partial class Form1 : Form
+    public partial class EburyPartners : Form
     {
-        public Form1()
+        public EburyPartners()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
