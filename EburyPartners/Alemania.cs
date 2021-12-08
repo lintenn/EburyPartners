3c12361ea3f15f9a1da7b5f2deb934c4de608e52
Luis <linten42@gmail.com>
2021-12-08 02:39:21 +0100

    Añadido BDLibrary y conexion a MySQL

--- EburyPartners/Alemania.cs
+++ EburyPartners/Alemania.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BDLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,11 @@ namespace EburyPartners
 {
     public partial class Alemania : Form
     {
+        private const string SERVER = "eburyrequisitos.cobadwnzalab.eu-central-1.rds.amazonaws.com";
+        private const string BD = "grupo01DB";
+        private const string USER = "grupo01";
+        private const string PWD = "nsB79maupU4rELd4";
+
         public Alemania()
         {
             InitializeComponent();
@@ -19,12 +25,23 @@ namespace EburyPartners
 
         private void bGenerarInformeInicial_Click(object sender, EventArgs e)
         {
+            try
+            {
+                MYSQLDB miBD = new MYSQLDB(SERVER, BD, USER, PWD);
+                string sql = (string)miBD.SelectScalar("SELECT DNI_NIF FROM Cliente WHERE DNI_NIF = 111111;");
 
+                tMessage.Text = sql;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+            
         }
 
         private void bGenerarInformeSemanal_Click(object sender, EventArgs e)
         {
-
+            tMessage.Text = "";
         }
 
         private void Alemania_Load(object sender, EventArgs e)
