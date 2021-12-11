35a2a7cfefbca647fa8fa194902f0e7ca882277c
Angelo <0619974249@uma.es>
2021-12-11 12:51:22 +0100

    a

--- EburyPartners/FiltroClientes.cs
+++ EburyPartners/FiltroClientes.cs
@@ -12,9 +12,26 @@ namespace EburyPartners
 {
     public partial class FiltroClientes : Form
     {
+        private const string SERVER = "eburyrequisitos.cobadwnzalab.eu-central-1.rds.amazonaws.com";
+        private const string BD = "grupo01DB";
+        private const string USER = "grupo01";
+        private const string PWD = "nsB79maupU4rELd4";
+
         public FiltroClientes()
         {
             InitializeComponent();
+            //cbActivas.DropDownStyle = ComboBoxStyle.DropDownList;
+            //cbActivas.Text = "Todas";
+        }
+
+        private void bBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tCuenta_MouseClick(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
