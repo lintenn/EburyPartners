7535cb9757a0e90e6209fb8662e9dfbdec1a5e21
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2021-12-12 21:45:16 +0100

    Se borra los archivos csv al enviarse por mail

--- EburyPartners/Alemania.Designer.cs
+++ EburyPartners/Alemania.Designer.cs
@@ -38,10 +38,10 @@
             // bGenerarInformeInicial
             // 
             this.bGenerarInformeInicial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bGenerarInformeInicial.Location = new System.Drawing.Point(368, 126);
-            this.bGenerarInformeInicial.Margin = new System.Windows.Forms.Padding(4);
+            this.bGenerarInformeInicial.Location = new System.Drawing.Point(414, 158);
+            this.bGenerarInformeInicial.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bGenerarInformeInicial.Name = "bGenerarInformeInicial";
-            this.bGenerarInformeInicial.Size = new System.Drawing.Size(325, 74);
+            this.bGenerarInformeInicial.Size = new System.Drawing.Size(366, 92);
             this.bGenerarInformeInicial.TabIndex = 0;
             this.bGenerarInformeInicial.Text = "Generar informe inicial";
             this.bGenerarInformeInicial.UseVisualStyleBackColor = true;
@@ -50,10 +50,10 @@
             // bGenerarInformeSemanal
             // 
             this.bGenerarInformeSemanal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bGenerarInformeSemanal.Location = new System.Drawing.Point(368, 277);
-            this.bGenerarInformeSemanal.Margin = new System.Windows.Forms.Padding(4);
+            this.bGenerarInformeSemanal.Location = new System.Drawing.Point(414, 346);
+            this.bGenerarInformeSemanal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bGenerarInformeSemanal.Name = "bGenerarInformeSemanal";
-            this.bGenerarInformeSemanal.Size = new System.Drawing.Size(325, 74);
+            this.bGenerarInformeSemanal.Size = new System.Drawing.Size(366, 92);
             this.bGenerarInformeSemanal.TabIndex = 1;
             this.bGenerarInformeSemanal.Text = "Generar informe semanal";
             this.bGenerarInformeSemanal.UseVisualStyleBackColor = true;
@@ -64,11 +64,11 @@
             this.tMessage.BackColor = System.Drawing.SystemColors.Control;
             this.tMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tMessage.Enabled = false;
-            this.tMessage.Location = new System.Drawing.Point(352, 447);
-            this.tMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.tMessage.Location = new System.Drawing.Point(396, 559);
+            this.tMessage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tMessage.Multiline = true;
             this.tMessage.Name = "tMessage";
-            this.tMessage.Size = new System.Drawing.Size(369, 54);
+            this.tMessage.Size = new System.Drawing.Size(415, 139);
             this.tMessage.TabIndex = 2;
             // 
             // bBack
@@ -81,10 +81,10 @@
             this.bBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bBack.ForeColor = System.Drawing.Color.Transparent;
             this.bBack.Image = ((System.Drawing.Image)(resources.GetObject("bBack.Image")));
-            this.bBack.Location = new System.Drawing.Point(879, 467);
-            this.bBack.Margin = new System.Windows.Forms.Padding(4);
+            this.bBack.Location = new System.Drawing.Point(989, 584);
+            this.bBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bBack.Name = "bBack";
-            this.bBack.Size = new System.Drawing.Size(108, 74);
+            this.bBack.Size = new System.Drawing.Size(122, 92);
             this.bBack.TabIndex = 7;
             this.bBack.UseMnemonic = false;
             this.bBack.UseVisualStyleBackColor = false;
@@ -92,15 +92,15 @@
             // 
             // Alemania
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.bBack);
             this.Controls.Add(this.tMessage);
             this.Controls.Add(this.bGenerarInformeSemanal);
             this.Controls.Add(this.bGenerarInformeInicial);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Alemania";
             this.Text = "Alemania";
             this.ResumeLayout(false);
