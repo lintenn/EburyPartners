a414a33f64ed2b7dc94eb0447b81d80f2f4898b5
Galo <galopg2001@hotmail.com>
2021-12-17 15:29:41 +0100

    Arreglo consulta filtro cliente.

--- EburyPartners/FiltroCuenta.cs
+++ EburyPartners/FiltroCuenta.cs
@@ -49,7 +49,7 @@ namespace EburyPartners
                 }
                 else
                 {
-                    consulta = "SELECT P.IBAN,P.caja_deposito,P.fecha_apertura, P.fecha_cierre, P.cantidad, P.moneda, P.pais, P.estado estado_cuenta, P.tipo tipo_cuenta, Pr.DNI_NIF, Pr.estado estado_usuario, Pr.tipo tipo_usuario, Pr.primer_nombre, Pr.segundo_nombre, Pr.ciudad, Pr.calle, Pr.num_edificio, Pr.codigo_postal, Pr.pais_cliente FROM Producto P JOIN Propietarios Prop ON P.IBAN = Prop.IBAN JOIN Cliente Pr ON Pr.DNI_NIF = Prop.DNI_NIF WHERE P.IBAN LIKE '%" + tCuenta.Text + "%' AND pais = 'NL' AND P.estado = '" + cbActivas.Text + ";";
+                    consulta = "SELECT P.IBAN,P.caja_deposito,P.fecha_apertura, P.fecha_cierre, P.cantidad, P.moneda, P.pais, P.estado estado_cuenta, P.tipo tipo_cuenta, Pr.DNI_NIF, Pr.estado estado_usuario, Pr.tipo tipo_usuario, Pr.primer_nombre, Pr.segundo_nombre, Pr.ciudad, Pr.calle, Pr.num_edificio, Pr.codigo_postal, Pr.pais_cliente FROM Producto P JOIN Propietarios Prop ON P.IBAN = Prop.IBAN JOIN Cliente Pr ON Pr.DNI_NIF = Prop.DNI_NIF WHERE P.IBAN LIKE '%" + tCuenta.Text + "%' AND pais = 'NL' AND P.estado = '" + cbActivas.Text + "';";
                 }
 
                 MYSQLDB miBD = new MYSQLDB(SERVER, BD, USER, PWD);
