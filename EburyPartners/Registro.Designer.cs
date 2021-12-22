3be1335198cdff8e91bf634bb9527aaa19ce632f
Galo <galopg2001@hotmail.com>
2021-12-22 02:55:00 +0100

    Registro avanzado

--- EburyPartners/Registro.Designer.cs
+++ EburyPartners/Registro.Designer.cs
@@ -35,16 +35,31 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lFecha = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.tNombre1 = new System.Windows.Forms.TextBox();
             this.tApellido2 = new System.Windows.Forms.TextBox();
             this.tApellido1 = new System.Windows.Forms.TextBox();
             this.tNombre2 = new System.Windows.Forms.TextBox();
-            this.tFecha = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tCP = new System.Windows.Forms.TextBox();
+            this.tCiudad = new System.Windows.Forms.TextBox();
+            this.tCalle = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tPais = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tNumero = new System.Windows.Forms.TextBox();
+            this.tDNI = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.bRegistro = new System.Windows.Forms.Button();
+            this.lr1 = new System.Windows.Forms.Label();
+            this.lr2 = new System.Windows.Forms.Label();
+            this.lr3 = new System.Windows.Forms.Label();
+            this.lr4 = new System.Windows.Forms.Label();
+            this.lStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // bBack
@@ -71,15 +86,16 @@
             this.cbTipo.Items.AddRange(new object[] {
             "Autónomo",
             "Empresa"});
-            this.cbTipo.Location = new System.Drawing.Point(357, 47);
+            this.cbTipo.Location = new System.Drawing.Point(357, 63);
             this.cbTipo.Name = "cbTipo";
             this.cbTipo.Size = new System.Drawing.Size(121, 21);
             this.cbTipo.TabIndex = 7;
+            this.cbTipo.SelectedIndexChanged += new System.EventHandler(this.cbTipo_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(286, 50);
+            this.label1.Location = new System.Drawing.Point(286, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 8;
@@ -88,16 +104,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 102);
+            this.label2.Location = new System.Drawing.Point(147, 158);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 9;
-            this.label2.Text = "Primer nombre:";
+            this.label2.Text = "Nombre:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(128, 133);
+            this.label3.Location = new System.Drawing.Point(103, 198);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 13);
             this.label3.TabIndex = 10;
@@ -106,25 +122,25 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(128, 191);
+            this.label4.Location = new System.Drawing.Point(103, 276);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(92, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Segundo apellido:";
             // 
-            // label5
+            // lFecha
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(111, 219);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(109, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Fecha de nacimiento:";
+            this.lFecha.AutoSize = true;
+            this.lFecha.Location = new System.Drawing.Point(85, 314);
+            this.lFecha.Name = "lFecha";
+            this.lFecha.Size = new System.Drawing.Size(109, 13);
+            this.lFecha.TabIndex = 12;
+            this.lFecha.Text = "Fecha de nacimiento:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(141, 162);
+            this.label6.Location = new System.Drawing.Point(116, 238);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(78, 13);
             this.label6.TabIndex = 13;
@@ -133,7 +149,7 @@
             // tNombre1
             // 
             this.tNombre1.ForeColor = System.Drawing.Color.Silver;
-            this.tNombre1.Location = new System.Drawing.Point(225, 99);
+            this.tNombre1.Location = new System.Drawing.Point(200, 155);
             this.tNombre1.Name = "tNombre1";
             this.tNombre1.Size = new System.Drawing.Size(166, 20);
             this.tNombre1.TabIndex = 14;
@@ -141,7 +157,7 @@
             // tApellido2
             // 
             this.tApellido2.ForeColor = System.Drawing.Color.Silver;
-            this.tApellido2.Location = new System.Drawing.Point(226, 188);
+            this.tApellido2.Location = new System.Drawing.Point(201, 273);
             this.tApellido2.Name = "tApellido2";
             this.tApellido2.Size = new System.Drawing.Size(166, 20);
             this.tApellido2.TabIndex = 15;
@@ -149,7 +165,7 @@
             // tApellido1
             // 
             this.tApellido1.ForeColor = System.Drawing.Color.Silver;
-            this.tApellido1.Location = new System.Drawing.Point(225, 159);
+            this.tApellido1.Location = new System.Drawing.Point(200, 235);
             this.tApellido1.Name = "tApellido1";
             this.tApellido1.Size = new System.Drawing.Size(166, 20);
             this.tApellido1.TabIndex = 16;
@@ -157,23 +173,15 @@
             // tNombre2
             // 
             this.tNombre2.ForeColor = System.Drawing.Color.Silver;
-            this.tNombre2.Location = new System.Drawing.Point(225, 130);
+            this.tNombre2.Location = new System.Drawing.Point(200, 195);
             this.tNombre2.Name = "tNombre2";
             this.tNombre2.Size = new System.Drawing.Size(166, 20);
             this.tNombre2.TabIndex = 17;
             // 
-            // tFecha
-            // 
-            this.tFecha.ForeColor = System.Drawing.Color.Silver;
-            this.tFecha.Location = new System.Drawing.Point(226, 219);
-            this.tFecha.Name = "tFecha";
-            this.tFecha.Size = new System.Drawing.Size(166, 20);
-            this.tFecha.TabIndex = 18;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(470, 102);
+            this.label7.Location = new System.Drawing.Point(443, 158);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(75, 13);
             this.label7.TabIndex = 19;
@@ -182,7 +190,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(470, 133);
+            this.label8.Location = new System.Drawing.Point(475, 198);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(43, 13);
             this.label8.TabIndex = 20;
@@ -191,11 +199,156 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(470, 162);
+            this.label9.Location = new System.Drawing.Point(485, 237);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(33, 13);
             this.label9.TabIndex = 21;
             this.label9.Text = "Calle:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(200, 314);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 22;
+            // 
+            // tCP
+            // 
+            this.tCP.ForeColor = System.Drawing.Color.Silver;
+            this.tCP.Location = new System.Drawing.Point(524, 155);
+            this.tCP.Name = "tCP";
+            this.tCP.Size = new System.Drawing.Size(166, 20);
+            this.tCP.TabIndex = 23;
+            // 
+            // tCiudad
+            // 
+            this.tCiudad.ForeColor = System.Drawing.Color.Silver;
+            this.tCiudad.Location = new System.Drawing.Point(524, 195);
+            this.tCiudad.Name = "tCiudad";
+            this.tCiudad.Size = new System.Drawing.Size(166, 20);
+            this.tCiudad.TabIndex = 24;
+            // 
+            // tCalle
+            // 
+            this.tCalle.ForeColor = System.Drawing.Color.Silver;
+            this.tCalle.Location = new System.Drawing.Point(524, 235);
+            this.tCalle.Name = "tCalle";
+            this.tCalle.Size = new System.Drawing.Size(166, 20);
+            this.tCalle.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(485, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(30, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Pais:";
+            // 
+            // tPais
+            // 
+            this.tPais.ForeColor = System.Drawing.Color.Silver;
+            this.tPais.Location = new System.Drawing.Point(524, 117);
+            this.tPais.Name = "tPais";
+            this.tPais.Size = new System.Drawing.Size(166, 20);
+            this.tPais.TabIndex = 27;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(471, 274);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Número:";
+            // 
+            // tNumero
+            // 
+            this.tNumero.ForeColor = System.Drawing.Color.Silver;
+            this.tNumero.Location = new System.Drawing.Point(524, 273);
+            this.tNumero.Name = "tNumero";
+            this.tNumero.Size = new System.Drawing.Size(166, 20);
+            this.tNumero.TabIndex = 29;
+            // 
+            // tDNI
+            // 
+            this.tDNI.ForeColor = System.Drawing.Color.Silver;
+            this.tDNI.Location = new System.Drawing.Point(200, 117);
+            this.tDNI.Name = "tDNI";
+            this.tDNI.Size = new System.Drawing.Size(166, 20);
+            this.tDNI.TabIndex = 31;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(142, 120);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(52, 13);
+            this.label12.TabIndex = 30;
+            this.label12.Text = "DNI_NIF:";
+            // 
+            // bRegistro
+            // 
+            this.bRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bRegistro.Location = new System.Drawing.Point(357, 357);
+            this.bRegistro.Name = "bRegistro";
+            this.bRegistro.Size = new System.Drawing.Size(91, 34);
+            this.bRegistro.TabIndex = 32;
+            this.bRegistro.Text = "Registrar";
+            this.bRegistro.UseVisualStyleBackColor = true;
+            this.bRegistro.Click += new System.EventHandler(this.bRegistro_Click);
+            // 
+            // lr1
+            // 
+            this.lr1.AutoSize = true;
+            this.lr1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lr1.ForeColor = System.Drawing.Color.Red;
+            this.lr1.Location = new System.Drawing.Point(368, 120);
+            this.lr1.Name = "lr1";
+            this.lr1.Size = new System.Drawing.Size(15, 20);
+            this.lr1.TabIndex = 33;
+            this.lr1.Text = "*";
+            // 
+            // lr2
+            // 
+            this.lr2.AutoSize = true;
+            this.lr2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lr2.ForeColor = System.Drawing.Color.Red;
+            this.lr2.Location = new System.Drawing.Point(368, 158);
+            this.lr2.Name = "lr2";
+            this.lr2.Size = new System.Drawing.Size(15, 20);
+            this.lr2.TabIndex = 35;
+            this.lr2.Text = "*";
+            // 
+            // lr3
+            // 
+            this.lr3.AutoSize = true;
+            this.lr3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lr3.ForeColor = System.Drawing.Color.Red;
+            this.lr3.Location = new System.Drawing.Point(368, 238);
+            this.lr3.Name = "lr3";
+            this.lr3.Size = new System.Drawing.Size(15, 20);
+            this.lr3.TabIndex = 36;
+            this.lr3.Text = "*";
+            // 
+            // lr4
+            // 
+            this.lr4.AutoSize = true;
+            this.lr4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lr4.ForeColor = System.Drawing.Color.Red;
+            this.lr4.Location = new System.Drawing.Point(406, 315);
+            this.lr4.Name = "lr4";
+            this.lr4.Size = new System.Drawing.Size(15, 20);
+            this.lr4.TabIndex = 37;
+            this.lr4.Text = "*";
+            // 
+            // lStatus
+            // 
+            this.lStatus.AutoSize = true;
+            this.lStatus.Location = new System.Drawing.Point(340, 402);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(0, 13);
+            this.lStatus.TabIndex = 38;
             // 
             // Registro
             // 
@@ -203,16 +356,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.lr4);
+            this.Controls.Add(this.lr3);
+            this.Controls.Add(this.lr2);
+            this.Controls.Add(this.lr1);
+            this.Controls.Add(this.bRegistro);
+            this.Controls.Add(this.tDNI);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.tNumero);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.tPais);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tCalle);
+            this.Controls.Add(this.tCiudad);
+            this.Controls.Add(this.tCP);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.tFecha);
             this.Controls.Add(this.tNombre2);
             this.Controls.Add(this.tApellido1);
             this.Controls.Add(this.tApellido2);
             this.Controls.Add(this.tNombre1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lFecha);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -234,15 +402,30 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lFecha;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox tNombre1;
         private System.Windows.Forms.TextBox tApellido2;
         private System.Windows.Forms.TextBox tApellido1;
         private System.Windows.Forms.TextBox tNombre2;
-        private System.Windows.Forms.TextBox tFecha;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox tCP;
+        private System.Windows.Forms.TextBox tCiudad;
+        private System.Windows.Forms.TextBox tCalle;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tPais;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tNumero;
+        private System.Windows.Forms.TextBox tDNI;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button bRegistro;
+        private System.Windows.Forms.Label lr1;
+        private System.Windows.Forms.Label lr2;
+        private System.Windows.Forms.Label lr3;
+        private System.Windows.Forms.Label lr4;
+        private System.Windows.Forms.Label lStatus;
     }
 }
