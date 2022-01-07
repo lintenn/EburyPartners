c4a30abd5dc1395e4eac34da53bcd5e0ac4f66de
Miguel <miguelog@uma.es>
2022-01-07 20:21:30 +0100

    Asociar cuenta

--- EburyPartners/AsociarCuenta.cs
+++ EburyPartners/AsociarCuenta.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BDLibrary;
 
 namespace EburyPartners
 {
     public partial class AsociarCuenta : Form
     {
+        private const string SERVER = "eburyrequisitos.cobadwnzalab.eu-central-1.rds.amazonaws.com";
+        private const string BD = "grupo01DB";
+        private const string USER = "grupo01";
+        private const string PWD = "nsB79maupU4rELd4";
+
         public AsociarCuenta()
         {
             InitializeComponent();
+            showClientes();
+        }
+
+        private void showClientes()
+        {
+            try
+            {     
+                MYSQLDB miBD = new MYSQLDB(SERVER, BD, USER, PWD);
+                miBD.mostrarDataGrid(dataGridViewClientes, "SELECT * FROM Cliente;");
+            }
+            catch (Exception)
+            {
+               
+            }
+        }
+
+        private void bBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
