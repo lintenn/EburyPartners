67f32f44a952d1358b36da8f531c7451eb2510e1
Ángel Joaquín Rodríguez Mercado <angelrodriguezmercado95@gmail.com>
2022-01-07 14:41:22 +0100

    Cambio de cuenta

---
+++ EburyPartners/CambioCuenta.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace EburyPartners
+{
+    partial class CambioCuenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CambioCuenta));
+            this.lNombre = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxTipoCuenta = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bBack = new System.Windows.Forms.Button();
+            this.lMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lNombre
+            // 
+            this.lNombre.AutoSize = true;
+            this.lNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNombre.Location = new System.Drawing.Point(251, 103);
+            this.lNombre.Name = "lNombre";
+            this.lNombre.Size = new System.Drawing.Size(233, 26);
+            this.lNombre.TabIndex = 0;
+            this.lNombre.Text = "El cliente con DNI/NIF ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cambiar cuenta a";
+            // 
+            // comboBoxTipoCuenta
+            // 
+            this.comboBoxTipoCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxTipoCuenta.FormattingEnabled = true;
+            this.comboBoxTipoCuenta.Location = new System.Drawing.Point(441, 289);
+            this.comboBoxTipoCuenta.Name = "comboBoxTipoCuenta";
+            this.comboBoxTipoCuenta.Size = new System.Drawing.Size(186, 34);
+            this.comboBoxTipoCuenta.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(375, 510);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cambiar cuenta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bBack
+            // 
+            this.bBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bBack.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.bBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bBack.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bBack.FlatAppearance.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.bBack.FlatAppearance.BorderSize = 10;
+            this.bBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bBack.ForeColor = System.Drawing.Color.Transparent;
+            this.bBack.Image = ((System.Drawing.Image)(resources.GetObject("bBack.Image")));
+            this.bBack.Location = new System.Drawing.Point(915, 490);
+            this.bBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bBack.Name = "bBack";
+            this.bBack.Size = new System.Drawing.Size(122, 92);
+            this.bBack.TabIndex = 66;
+            this.bBack.UseMnemonic = false;
+            this.bBack.UseVisualStyleBackColor = false;
+            this.bBack.Click += new System.EventHandler(this.bBack_Click);
+            // 
+            // lMessage
+            // 
+            this.lMessage.AutoSize = true;
+            this.lMessage.Location = new System.Drawing.Point(375, 579);
+            this.lMessage.Name = "lMessage";
+            this.lMessage.Size = new System.Drawing.Size(0, 30);
+            this.lMessage.TabIndex = 67;
+            // 
+            // CambioCuenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.lMessage);
+            this.Controls.Add(this.bBack);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxTipoCuenta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lNombre);
+            this.Name = "CambioCuenta";
+            this.Text = "Cambio de cuenta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxTipoCuenta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bBack;
+        private System.Windows.Forms.Label lMessage;
+    }
+}
