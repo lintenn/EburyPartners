b790c1a136b955ba26f35030ec5c9f4f75a19201
Miguel <miguelog@uma.es>
2022-01-18 10:04:11 +0100

    Elección registro

---
+++ EburyPartners/RegistroEmpresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EburyPartners
+{
+    public partial class RegistroEmpresa : Form
+    {
+        public RegistroEmpresa()
+        {
+            InitializeComponent();
+        }
+    }
+}
